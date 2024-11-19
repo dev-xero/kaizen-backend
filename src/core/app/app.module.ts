d6e4e29c144544f57a8fe44b5f96da01bdd1927c
= <ssh.xero@gmail.com>
2024-11-19 22:20:43 +0100

    feat: Add cors configuration

--- src/core/app/app.module.ts
+++ src/core/app/app.module.ts
@@ -1,10 +1,23 @@
+import corsOptions from '@config/cors';
 import { env } from '@config/variables';
 import logger from '@utils/logger';
+import compression from 'compression';
+import cors from 'cors';
 import express from 'express';
+import helmet from 'helmet';
 
 export async function StartApplication() {
     const app = express();
     const port = env.app.port;
+
+    app.disable('x-powered-by');
+
+    app.use(helmet());
+    app.use(compression());
+    app.use(cors(corsOptions));
+    app.use(express.urlencoded({ extended: false }));
+
+    app.use(express.json());
 
     app.listen(port, () =>
         logger.info(

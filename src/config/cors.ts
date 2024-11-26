14e950bd7f917f711f58fa7dabecf8596aec6730
= <ssh.xero@gmail.com>
2024-11-26 19:06:53 +0100

    fix(cors): Allow client url to connect

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,5 +1,7 @@
+import { env } from "./variables";
+
 const corsOptions = {
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', env.app.clientURL],
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 };

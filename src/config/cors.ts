6f551f19d570e91fb4c97346c8439eebded04c9f
= <ssh.xero@gmail.com>
2024-11-26 19:09:12 +0100

    fix(cors): Check if client url exists in cors config

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,7 +1,14 @@
-import { env } from "./variables";
+import { InternalServerError } from '@errors/internal.error';
+import { env } from './variables';
+
+const clientURL = env.app.clientURL;
+
+if (!clientURL) {
+    throw new InternalServerError('Client URL is not configured.');
+}
 
 const corsOptions = {
-    origin: ['http://localhost:3000', env.app.clientURL],
+    origin: ['http://localhost:3000', clientURL],
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 };
